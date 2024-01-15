87b51e985b52b865640f95e748bc9d564f414e5a
odhiambow2354 <odhiambow2354@gmail.com>
2024-01-15 23:11:46 +0300

    Python - Async

---
+++ 0x00-python_variable_annotations/0-add.py/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic add annotations
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    function tha adds two floats
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/0-add.py/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    returns a floor of the number
+    """
+    return int(n)
